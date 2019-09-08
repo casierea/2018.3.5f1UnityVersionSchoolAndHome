0053e6e1df69ddfc261d4b314bdcfd586023b7b4
Unknown <dancingmongoose@gmail.com>
2019-09-08 15:01:56 -0600

    Script GetKeyDown Space = true

---
+++ CSFall2019SchoolAndCasa/Assets/Scripts/CubeAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeAnimation : MonoBehaviour
+{
+   private Animator animator;
+   private bool tutorialanimating = false;
+
+   void Start()
+   {
+      animator = GetComponent<Animator>();
+   }
+
+   void Update()
+   {
+      if (Input.GetKeyDown(KeyCode.Space)&& !tutorialanimating)
+      {
+         animator.SetBool("tutorialanimating", true);
+         tutorialanimating = true;
+      }
+     else if (Input.GetKeyDown(KeyCode.Space)&& tutorialanimating)
+      {
+         animator.SetBool("tutorialanimating",false);
+         tutorialanimating = false;
+      }
+   }
+}
