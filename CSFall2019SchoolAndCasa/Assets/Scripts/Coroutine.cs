9de53d90bca4cb52d57ad2d6e570bca90635086f
Unknown <dancingmongoose@gmail.com>
2019-09-08 15:50:25 -0600

    Coroutine Tutorial

---
+++ CSFall2019SchoolAndCasa/Assets/Scripts/Coroutine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coroutine : MonoBehaviour
+{
+    [SerializeField] private GameObject Box;
+    [SerializeField] private int timer;
+
+
+
+    private void Start()
+    {
+        StartCoroutine(HideObject());
+    }
+    IEnumerator HideObject()
+    {
+        yield return new WaitForSeconds(timer);
+        Box.SetActive(false);
+    }
+}
