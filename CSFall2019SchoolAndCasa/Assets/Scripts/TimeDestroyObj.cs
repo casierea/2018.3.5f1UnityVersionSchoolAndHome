09d0ea725c56803566b7f34b6c188a60f8ac4434
Unknown <dancingmongoose@gmail.com>
2019-10-04 20:06:39 -0600

    Destroy obj script

---
+++ CSFall2019SchoolAndCasa/Assets/Scripts/TimeDestroyObj.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeDestroyObj : MonoBehaviour
+{
+    public float objLastsTime = 3;
+    
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(objLastsTime);
+        Destroy(gameObject);
+    }
+}
+   
