8e9786b69d1b42f7c93a708336d7db23d5249f28
Casie <dancingmongoose@gmail.com>
2019-10-07 14:32:33 -0600

    collections script

---
+++ CSFall2019SchoolAndCasa/Assets/Scripts/Collections.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class Collections : ScriptableObject
+{
+   public List<Object> objectlist;
+   public List<FloatData> FloatDatas;
+   public List<GameObject> GameObjects;
+   
+   public void findObjectType(object obj)
+   {
+      foreach (var currentObj in objectlist)
+      {
+         if (currentObj == obj)
+         {
+            //Do work; see if you have a match with a particular obj.
+            //instanticate, add to collection, use a speficic method to call.
+         }
+         
+      }
+   }
+   public void AddToList(Object obj)
+   {
+      objectlist.Add(obj);
+      //can add prefab in scene. added to collection. trigger event script, add + and add list. 
+      //could have a list of collected and not collected.
+   }
+
+   public void RemovefromList(Object obj)
+   {
+      foreach (var currentObj in objectlist)
+      {
+         objectlist.Remove(obj);
+      }
+   }
+}
