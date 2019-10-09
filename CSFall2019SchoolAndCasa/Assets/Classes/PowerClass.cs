163a09038a32c1f4543ecdae876a6ef2d1ca64c7
Casie <dancingmongoose@gmail.com>
2019-10-09 14:19:57 -0600

    Class EndGame Actions

---
+++ CSFall2019SchoolAndCasa/Assets/Classes/PowerClass.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerClass : MonoBehaviour
+{
+    public void EndPower()
+    {
+        print("End powerclass");
+    }
+}
