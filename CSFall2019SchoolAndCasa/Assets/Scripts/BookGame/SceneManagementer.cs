70425fd22f53f853a2c480269540e70d96ce78c8
Unknown <dancingmongoose@gmail.com>
2019-10-17 19:11:32 -0600

    Title Page

---
+++ CSFall2019SchoolAndCasa/Assets/Scripts/BookGame/SceneManagementer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagementer : MonoBehaviour
+{
+   public void SceneGo()
+   {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+   }
+}
