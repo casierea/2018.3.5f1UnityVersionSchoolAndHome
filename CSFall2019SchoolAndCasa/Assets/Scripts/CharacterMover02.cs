04b9d03a8394655a51ab63963794a01509cf339a
Unknown <dancingmongoose@gmail.com>
2019-10-18 12:57:16 -0600

    fixed character mover script

--- CSFall2019SchoolAndCasa/Assets/Scripts/CharacterMover02.cs
+++ CSFall2019SchoolAndCasa/Assets/Scripts/CharacterMover02.cs
@@ -4,24 +4,17 @@ using UnityEngine;
 [CreateAssetMenu]
 public class CharacterMover02 : ScriptableObject
 {
-    public float moveSpeed = 10f, jumpSpeed = 30f, gravity = 3f;
+    private CharacterController controller;
     private Vector3 position;
-    public IntData jumpData;
-    
-    public void MoveCharacter(CharacterController controller)
+    private int jumpCount;
+    public float moveSpeed = 10f;
+
+    public void Move(CharacterController controller)
     {
         position.x = moveSpeed * Input.GetAxis("Horizontal");
         position.z = moveSpeed * Input.GetAxis("Vertical");
-        position.y -= gravity;
-
-        if (Input.GetButton("Jump") && jumpData.value < jumpData.maxValue)
-        {
-            position.y = jumpSpeed;
-            jumpData.value++;
-        } else if (controller.isGrounded)
-        {
-            position.y = 0;
-        }
+        
         controller.Move(position * Time.deltaTime);
+        
     }
 }
