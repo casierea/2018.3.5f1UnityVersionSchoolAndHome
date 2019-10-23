3b4e3ac4db9fde5fdc8fe92034ff61ae4082ca69
Casie <dancingmongoose@gmail.com>
2019-10-23 14:52:30 -0600

    Interfaces. Change move direction

--- CSFall2019SchoolAndCasa/Assets/Scripts/CharacterMover02.cs
+++ CSFall2019SchoolAndCasa/Assets/Scripts/CharacterMover02.cs
@@ -1,20 +1,26 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu]
 public class CharacterMover02 : ScriptableObject
 {
-    private CharacterController controller;
+    public float moveSpeed = 10f, jumpSpeed = 30f, gravity = 3f;
     private Vector3 position;
-    private int jumpCount;
-    public float moveSpeed = 10f;
-
-    public void Move(CharacterController controller)
+    public IntData jumpData;
+    
+    public void MoveCharacter(CharacterController controller)
     {
         position.x = moveSpeed * Input.GetAxis("Horizontal");
         position.z = moveSpeed * Input.GetAxis("Vertical");
-        
+        position.y -= gravity;
+
+        if (Input.GetButton("Jump") && jumpData.value < jumpData.maxValue)
+        {
+            position.y = jumpSpeed;
+            jumpData.value++;
+        } else if (controller.isGrounded)
+        {
+            position.y = 0;
+        }
         controller.Move(position * Time.deltaTime);
-        
     }
 }
--- CSFall2019SchoolAndCasa/Assets/Scripts/Interfaces/Interfaces.cs
+++ CSFall2019SchoolAndCasa/Assets/Scripts/Interfaces/Interfaces.cs
@@ -23,3 +23,10 @@ public interface ITrigger
 {
     void OnTriggerEnter(Collider obj);
 }
+
+
+public interface IMove
+{
+    Vector3 location { get; set; }
+    void Move(CharacterController controller);
+}
