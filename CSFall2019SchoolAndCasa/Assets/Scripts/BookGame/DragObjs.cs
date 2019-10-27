57b5260d5657e1442ccd64865f66cbb6ad7f7d94
Unknown <dancingmongoose@gmail.com>
2019-10-27 13:49:53 -0600

    drag and dop. wrong item disappears when collide

---
+++ CSFall2019SchoolAndCasa/Assets/Scripts/BookGame/DragObjs.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class DragObjs : MonoBehaviour
+{
+    public bool draggy = false;
+    public bool collisionOn = false;
+    private Vector3 position;
+    
+   
+    public void startDrag()
+    {
+        position = gameObject.transform.position;
+        draggy = true;
+    }
+
+    public void drag()
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void drop()
+    {
+        if (!collisionOn)
+        {
+            gameObject.transform.position = position;
+        }
+
+        draggy = false;
+    }
+}
---
+++ CSFall2019SchoolAndCasa/Assets/Scripts/BookGame/HoldContainer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class HoldContainer : MonoBehaviour
+//{
+
+   // void onCollisionStay(Collision col)
+   // {
+    //    if (col.collider.gameObject.tag == "object")
+    //    {
+      //      col.collider.GetComponent<object>().collision = true;
+      //      if (!col.collider.GetComponent<object>().dragging)
+       //     {
+       //         col.collider.gameObject.transform.position = transform.position;
+       //         Destroy(col.collider.gameObject);
+       //     }
+       // }
+   // }
+
+   // private void OnCollisionExit(Collision col)
+   // {
+   //     col.collider.GetComponent<object>().collision = false;
+   // }
+//}
