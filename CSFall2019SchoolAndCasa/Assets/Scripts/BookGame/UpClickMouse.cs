8ca3d17f5648feaf5082a9b46d8372c3eefbe9f3
Unknown <dancingmongoose@gmail.com>
2019-10-26 19:54:03 -0600

    OnMouseUp

---
+++ CSFall2019SchoolAndCasa/Assets/Scripts/BookGame/UpClickMouse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UpClickMouse : MonoBehaviour
+{
+    public UnityEvent OnMouseUpEvent;
+
+    private void OnMouseUp()
+    {
+        OnMouseUpEvent.Invoke();
+    }
+}
+
+
+
+  
