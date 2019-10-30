9d679918d685082dbd8690506146c6fe4ec59ec4
Casie <dancingmongoose@gmail.com>
2019-10-30 16:45:44 -0600

    Class character controller

---
+++ CSFall2019SchoolAndCasa/Assets/Scripts/CharacterPrime.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+[CreateAssetMenu]
+public class CharacterPrime : CharacterBase
+{
+    public override void Move()
+    {
+        Location.Set(Input.GetAxis("Vertical")*speed,0,0 );
+        Orientation.y = Input.GetAxis("Horizontal")*orientSpeed;
+        
+        Controller.transform.Rotate(Orientation);
+        Location = Controller.transform.TransformDirection(Location);
+        
+        Controller.Move(Location * Time.deltaTime);
+    }
+}
+
