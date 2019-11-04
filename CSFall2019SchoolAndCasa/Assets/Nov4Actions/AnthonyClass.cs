1a8daa78f5b3a746d4c4fcef10653cda58934761
Casie <dancingmongoose@gmail.com>
2019-11-04 14:38:09 -0700

    Gameaction, Marty, Anthony, Rodayne

---
+++ CSFall2019SchoolAndCasa/Assets/Nov4Actions/AnthonyClass.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class AnthonyClass : MonoBehaviour
+{
+    public GameAction gameActionObj;
+    
+    public void Yell()
+    {
+        Debug.Log("Anthony");
+    }
+    
+    private void Awake()
+    {
+        
+    }
+    
+    void Start()
+    {
+        gameActionObj.Raise();
+    }
+}
