679081a5d78cd94b6659b27fbaee4b2ed01ea2d8
Casie <dancingmongoose@gmail.com>
2019-11-06 14:24:39 -0700

    Dialog, character move jump, flip, interfaces

---
+++ CSFall2019SchoolAndCasa/Assets/CharacterBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CharacterBase : ScriptableObject
+{
+    public float moveSpeed = 3;
+    public float gravity = 9.81f;
+    public float jumpSpeed = 75f;
+    public CharacterController Controller { get; set; }
+    protected Vector3 Location;
+    protected Vector3 Orientation;
+
+
+
+    public abstract void Move();
+
+}
---
+++ CSFall2019SchoolAndCasa/Assets/Scripts/Interfaces/Flip.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flip : MonoBehaviour
+{
+    private Vector3 orientation;
+
+    private void Flipper (float direction)
+    {
+        orientation.y = direction;
+        transform.rotation = Quaternion.Euler(orientation);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow)) Flipper(direction: 180);
+        if(Input.GetKeyDown(KeyCode.RightArrow)) Flipper(direction: 0);
+    }
+}
+
