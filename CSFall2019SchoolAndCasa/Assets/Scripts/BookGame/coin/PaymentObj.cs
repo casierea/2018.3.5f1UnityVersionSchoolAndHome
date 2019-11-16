b111293cb26f91ef0b899c542813a0ebb1138293
Unknown <dancingmongoose@gmail.com>
2019-11-15 20:57:50 -0700

    coin script practice

---
+++ CSFall2019SchoolAndCasa/Assets/Scripts/BookGame/coin/PaymentObj.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class PaymentObj : ScriptableObject
+{//need value to be able to put coin.value
+
+    public UnityAction<int> CostAction;
+    public int value = 1;
+   
+    public void UseIt()
+    {
+        CostAction?.Invoke(value);
+    }
+}
