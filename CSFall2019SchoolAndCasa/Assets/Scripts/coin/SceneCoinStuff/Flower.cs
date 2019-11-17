8b011d2ef0a4a45e1cc7551bcd325722e5403f6a
Unknown <dancingmongoose@gmail.com>
2019-11-16 19:18:52 -0700

    flower like coin SO

---
+++ CSFall2019SchoolAndCasa/Assets/Scripts/coin/SceneCoinStuff/Flower.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class Flower : ScriptableObject
+{//need value to be able to put coin.value
+
+    public UnityAction<int> flowerAction;
+    public int value = 50;
+   
+    public void Spend()
+    {
+        flowerAction?.Invoke(value);
+    }
+}
