0557c4c4475e38ac329d2a5b515f66360290b089
Unknown <dancingmongoose@gmail.com>
2019-11-26 15:48:49 -0700

    Scripts from nov 20

---
+++ CSFall2019SchoolAndCasa/Assets/Scripts/BookGame/ButtonFire/AmHand02.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class AmHand02 : MonoBehaviour
+{
+    private Rigidbody rigidbodyObj;
+    public Vector3 Forces;
+    public WeaponConfig weaponObj;
+    
+    void Start()
+    {
+        var renderer = GetComponent<Renderer>();
+        renderer.material.color = weaponObj.weaponColor;
+        rigidbodyObj = GetComponent<Rigidbody>();
+        rigidbodyObj.AddForce(Forces);
+        Destroy(gameObject, 1f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        weaponObj.dodamage();
+    }
+}
