7e8c6f0917911fa1b2068b56e757ebd3b77739d7
Unknown <dancingmongoose@gmail.com>
2019-11-30 15:40:51 -0700

    raycast flashlight obj

---
+++ CSFall2019SchoolAndCasa/Assets/Scripts/BookGame/ManageSelection.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageSelection : MonoBehaviour
+{
+    [SerializeField] private string selectableTag = "PickMe";
+    [SerializeField] private Material flashlightMaterial;
+    [SerializeField] private Material normalMaterial;
+
+    private Transform Aselection;
+    
+    private void Update()
+    {
+        if (Aselection != null)
+        {
+            var selectionRenderer = Aselection.GetComponent<Renderer>();
+            selectionRenderer.material = normalMaterial;
+            Aselection = null;
+        }
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var selection = hit.transform;
+            if (selection.CompareTag(selectableTag))
+            {
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+                    selectionRenderer.material = flashlightMaterial;
+                }
+
+                Aselection = selection;
+            }
+            
+        }
+    }
+}
