a62223fa608488f88711f0a003a44bc00b1cd4cd
Unknown <dancingmongoose@gmail.com>
2019-11-30 14:31:07 -0700

    raycast experiment script. camera on flashlight

---
+++ CSFall2019SchoolAndCasa/Assets/Scripts/BookGame/CastTheRay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastTheRay : MonoBehaviour
+{
+    public float Distance;
+    
+    void Update()
+    {
+        RaycastHit Hit;
+        
+        if(Physics.Raycast (transform.position, transform.TransformDirection( Vector3.forward), out Hit))
+        {
+            
+        }
+    }
+}
