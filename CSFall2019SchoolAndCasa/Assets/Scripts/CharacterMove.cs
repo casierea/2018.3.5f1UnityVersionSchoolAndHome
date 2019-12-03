6c4fc2b9a59f03ecea90559908f813ebec3fdb85
Casie <dancingmongoose@gmail.com>
2019-12-03 14:17:52 -0700

    Dialog box works

---
+++ CSFall2019SchoolAndCasa/Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMove : MonoBehaviour
+{
+    private CharacterController Controller;
+
+    public float Gravity = 9.81f;
+    public float MoveSpeed = 3.0f;
+    public float JumpSpeed = 4.0f;
+    private Vector3 rot;
+
+
+    private Vector3 position;
+    //private Vector3 rotation;
+
+    void Start()
+    {
+        Controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (Controller.isGrounded)
+        {
+            position.Set(MoveSpeed * Input.GetAxis("Horizontal"), 0, MoveSpeed * Input.GetAxis("Vertical"));
+            // rotation.Set(0, Input.GetAxis("Vertical"), 0);
+            // transform.Rotate(rotation);
+            position = transform.TransformDirection(position);
+            print(position);
+
+            if (Input.GetButton("Jump"))
+            {
+                position.y = JumpSpeed;
+            }
+        }
+      
+
+        position.y -= Gravity * Time.deltaTime;
+        Controller.Move(position * Time.deltaTime);
+       
+    }
+    
+    
+
+}
