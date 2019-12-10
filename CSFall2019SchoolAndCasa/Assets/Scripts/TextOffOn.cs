79aaa090938f6ba41a5b6af4da6edb1cce62a02f
Unknown <dancingmongoose@gmail.com>
2019-12-10 13:31:10 -0700

    text try script. organize pages. delete slide page.

---
+++ CSFall2019SchoolAndCasa/Assets/Scripts/TextOffOn.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextOffOn : MonoBehaviour
+{
+
+   public void OnOffText(UnityEngine.UI.Text text)
+   {
+      text.gameObject.active = !text.gameObject.active;
+   }
+}
